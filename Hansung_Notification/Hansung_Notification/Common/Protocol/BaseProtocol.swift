b4fa063fb0795200a0daba61005038d398209950
chansenbbai <chanchu@hansung.ac.kr>
2022-07-26 14:54:10 +0900

    [Add] BaseProtocol (#14)

---
+++ Hansung_Notification/Hansung_Notification/Common/Protocol/BaseProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  BaseProtocol.swift
+//  Hansung_Notification
+//
+//  Created by 김승찬 on 2022/07/26.
+//
+
+import Foundation
+
+protocol DelegationProtocol {
+    func assignDelegation()
+}
+
+protocol TargetProtocol {
+    func addTarget()
+}
